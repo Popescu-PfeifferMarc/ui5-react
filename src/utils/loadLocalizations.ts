f12c3d829b4b2c167d861aac502c11c5d3bb413c
Marc Popescu-Pfeiffer <marc.popescu-pfeiffer@integra-ev.de>
2022-11-13 12:15:03 +0100

    added missing asset files

--- src/utils/loadLocalizations.ts
+++ src/utils/loadLocalizations.ts
@@ -1,13 +1,14 @@
 // @ts-expect-error
 import { registerI18nLoader } from '@ui5/webcomponents-base/dist/asset-registries/i18n.js';
 
-import '@ui5/webcomponents-icons/dist/Assets.js';
+import '@ui5/webcomponents/dist/Assets.js';
 import '@ui5/webcomponents-fiori/dist/Assets.js';
+import '@ui5/webcomponents-icons/dist/Assets.js';
+import '@ui5/webcomponents-react/dist/Assets.js';
 
 const loadLocalizations = (appName: string) =>
 	['de', 'en'].forEach((lang) =>
 		registerI18nLoader(appName, lang, async () => import(`../assets/i18n/messagebundle_${lang}.json`)),
 	);
-
 
 export default loadLocalizations;
